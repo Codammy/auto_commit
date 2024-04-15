00212b31083a03264187be98cc7b2ab63e7d0d10
Codam <damisco005@gmail.com>
2024-04-15 04:06:26 +0100

    Created test

--- auto_commit.sh
+++ auto_commit.sh
@@ -6,21 +6,26 @@ LASTMOD=$(date -r .);
 # alias commit="git commit -m";
 # alias add="git add";
 
+
 if [ -e .git ]; then
+
+	if [ "$1" != "-o" -a "$2" == '' ]; then
+		$2="/dev/null";
+	fi
 	while [ 1 ]; do
 		MOD=$(date -r $(ls -t | head -1))
 		if [ "$MOD" != "$LASTMOD" ]; then
 			git add .
 			DIRLEN=$(ls | wc -l)
 			if [[ "$FILECOUNT" -lt "$DIRLEN" ]]; then
-				git commit -m "Created $(ls -t | head -1)" >> commit_history
+				git commit -m "Created $(ls -t | head -1)" >> $2;
 			elif [[ "$FILECOUNT" -gt "$DIRLEN" ]]; then
-				git commit -m "Removed a file" >> commit_history
+				git commit -m "Removed a file" >> $2;
 			else
 				if [ "$AUTOCOMMITMSG" == '' ]; then
 					AUTOCOMMITMSG="Made changes to to $(ls -t | head -1)";
 				fi
-				git commit -m "$AUTOCOMMITMSG" >> commit_history
+				git commit -m "$AUTOCOMMITMSG" >> $2;
 			fi
 			FILECOUNT=$DIRLEN;
 			LASTMOD=$MOD;
