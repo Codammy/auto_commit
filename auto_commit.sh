25f8cb9d768cbc66c3c828f1cff256ae48668e48
Codam <damisco005@gmail.com>
2024-04-16 07:24:45 +0100

    Created auto_commit.sh

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 # automatically commits a change made on a file when saved
 
+if [ -ne .git ]; then
+	echo "Not a git repo! run git init to get started"
+	exit 1;
+fi
+
 FIlECOUNT=$(ls | wc -l);
 LASTMOD=$(date -r .);
+O='.commit_history'
+
 # alias commit="git commit -m";
 # alias add="git add";
 
-
-if [ -e .git ]; then
-	O='.commit_history'
-	while [ 1 ]; do
-		MOD=$(date -r $(ls -t | head -1))
-		if [ "$MOD" != "$LASTMOD" ]; then
-			git add .
-			DIRLEN=$(ls | wc -l)
-			if [[ "$FILECOUNT" -lt "$DIRLEN" ]]; then
-				git commit -m "Created $(ls -t | head -1)" >> $O;
-			elif [[ "$FILECOUNT" -gt "$DIRLEN" ]]; then
-				git commit -m "Removed a file" >> $O;
-			else
-				if [ "$AUTOCOMMITMSG" == '' ]; then
-					AUTOCOMMITMSG="Made changes to to $(ls -t | head -1)";
-				fi
-				git commit -m "$AUTOCOMMITMSG" >> $O;
+while [ -e .git ]; do
+	MOD=$(date -r $(ls -t | head -1))
+	if [ "$MOD" != "$LASTMOD" ]; then
+		git add .
+		DIRLEN=$(ls | wc -l)
+		if [[ "$FILECOUNT" -lt "$DIRLEN" ]]; then
+			git commit -m "Created $(ls -t | head -1)" >> $O;
+		elif [[ "$FILECOUNT" -gt "$DIRLEN" ]]; then
+			git commit -m "Removed a file" >> $O;
+		else
+			if [ "$AUTOCOMMITMSG" == '' ]; then
+				AUTOCOMMITMSG="Made changes to $(ls -t | head -1)";
 			fi
-			FILECOUNT=$DIRLEN;
-			LASTMOD=$MOD;
+			git commit -m "$AUTOCOMMITMSG" >> $O;
 		fi
-	done;
-else
-	echo "Not a git repo! run git init to get started"
-fi
+		FILECOUNT=$DIRLEN;
+		LASTMOD=$MOD;
+	fi
+done;
